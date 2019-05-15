60fa06194263fc1c34aec69edb3cf29d9bdd509c
鳗鱼不是鱼 <951086294@qq.com>
2019-05-15 22:47:03 +0800

    重新更改

--- src/main/java/com/black/mapper/OrderMapper.java
+++ src/main/java/com/black/mapper/OrderMapper.java
@@ -9,5 +9,13 @@ import java.util.List;
  * @date 2019/3/18 14:03
  */
 public interface OrderMapper {
+    //获取所有订单列表
     List<Order> querryAll();
+
+    //更新猫
+    int updataorder(Order order);
+    //删除猫
+    int deleteorder(Order order);
+    //添加猫
+    int addorder(Order order);
 }
